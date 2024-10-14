e9911d4869281fa2b80fb3b0f2e34ae12297111c
deekshareddy57 <deekshareddy.c@gmail.com>
2024-10-14 11:15:44 -0500

    commit 1

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='flask/templates')
+app = Flask(__name__, template_folder='flask/templates', static_folder='flask/static')
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/project/<int:project_id>')
+def project_detail(project_id):
+    projects = {
+        1: {'name': 'Project One', 'description': 'Description of Project One'},
+        2: {'name': 'Project Two', 'description': 'Description of Project Two'}
+    }
+    project = projects.get(project_id, None)
+    if project:
+        return render_template('project_detail.html', project=project)
+    else:
+        return render_template('404.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
