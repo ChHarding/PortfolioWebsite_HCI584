c371ff48ce22793ee948756c0849441344a1ed1d
deekshareddy57 <deekshareddy.c@gmail.com>
2024-11-04 19:28:49 -0600

    -created the login for admin through the top navigation
    -added the logout feature
    
    -adding the project from admin mode is defined and easier.
    
    -changed the layout, structure, styling of the whole web application to make it more user friendly
    
    -restructured the home and add_project class.

--- main.py
+++ main.py
@@ -32,6 +32,13 @@ def login():
 
     return render_template('login.html')  # Render login form
 
+@app.route('/logout')
+def logout():
+    # Clear all session data
+    session.clear()
+    flash('You have been logged out successfully.')
+    return redirect(url_for('home'))
+
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///projects.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -52,61 +59,13 @@ class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=False)
-    image = db.Column(db.String(100))
+    image = db.Column(db.String(200))
     pdf = db.Column(db.String(100))
     link = db.Column(db.String(200))
 
     def __repr__(self):
         return f'<Project {self.name}>'
 
-#@app.route('/project_files/<path:filename>')
-#def project_files(filename):
-#    # Serve files from the 'projects' directory
-#    return send_from_directory(os.path.join(app.root_path, 'projects'), filename)
-
-
-'''
-@app.route('/')
-def home():
-    project_dir = os.path.join(app.root_path, 'projects')
-    projects = []
-
-    # Loop through each subfolder in the projects directory
-    if os.path.exists(project_dir):
-        for project_folder in os.listdir(project_dir):
-            folder_path = os.path.join(project_dir, project_folder)
-            if os.path.isdir(folder_path):
-                # Read project files (e.g., description.txt, image, pdf)
-                description_path = os.path.join(folder_path, 'description.txt')
-                image_filename = 'project_image.jpg'
-                pdf_filename = 'project_document.pdf'
-                link_path = os.path.join(folder_path, 'link.txt')
-
-                if os.path.exists(description_path):
-                    with open(description_path, 'r') as f:
-                        description = f.read()
-                else:
-                    description = 'No description available.'
-
-                if os.path.exists(link_path):
-                    with open(link_path, 'r') as f:
-                        link = f.read()
-
-                # Check for image and PDF existence
-                image_path = f'project_files/{project_folder}/{image_filename}' if os.path.exists(os.path.join(folder_path, image_filename)) else None
-                pdf_path = f'project_files/{project_folder}/{pdf_filename}' if os.path.exists(os.path.join(folder_path, pdf_filename)) else None
-
-                # Add to projects list
-                projects.append({
-                    'name': project_folder,
-                    'description': description,
-                    'image': image_path,
-                    'link': link,
-                    'pdf': pdf_path
-                })
-
-    return render_template('index.html', projects=projects)
-'''
 
 @app.route('/')
 def home():
